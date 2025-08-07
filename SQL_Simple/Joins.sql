28bd721913af5285e72b73d2c0f4ae469e8a649d
AjayAyrekar <ajayayrekaraj@gmail.com>
2025-08-07 15:37:58 +0530

    Update

--- SQL_Simple/Joins.sql
+++ SQL_Simple/Joins.sql
@@ -1,2 +1,14 @@
 # JOINS
+-- whenever we want to combine two tables or extract data from multiple tables then we use join
 
+-- Types of Joins
+-- 1 . Inner Join
+-- 2 . Outer Join  - 1 .Left outer join(left join)
+				-- - 2 .Right outer join(Right join)
+                -- - 3 .Full outer join (Full join)
+                
+ -- Natural joins , self join and cross join
+ 
+ -- 1 .Inner Join - common part between the tables based on the common column
+ 
+ 
