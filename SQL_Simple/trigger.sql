6c12e3d053e71c1d8a66cad562aa3bda3002b689
AjayAyrekar <ajayayrekaraj@gmail.com>
2025-09-04 17:52:45 +0530

    Trigger

---
+++ SQL_Simple/trigger.sql
@@ -0,0 +1,29 @@
+# trigger
+
+use practise_sql;
+
+create table student(
+name varchar(13),
+sem1 float,
+sem2 float,
+sem3 float,
+avg float
+);
+
+delimiter $$
+create trigger avg_marks
+before insert
+on student
+for each row
+begin
+    set new.avg=(new.sem1+new.sem2+new.sem3)/3;
+end
+$$
+
+insert into student values('A',80,70,60,0);
+
+select * from student;
+
+alter table student add id int;
+
+
